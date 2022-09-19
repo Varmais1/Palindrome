f3a9f70d6f4580c8f96a835e1e653bab15a218e9
Varmais1 <418487@bsd48.org>
2022-09-19 14:26:58 -0700

    not working, but comparisons added

--- palindrome.cpp
+++ palindrome.cpp
@@ -13,29 +13,40 @@ int main() {
   char reverse[80];
   int size = 0;
   int index = 0;
+  int loop = 0;
+  
+  bool nullcheck = false;
   for(int i = 0; i < 80; i++) {
     int character = (int) input[i];
-    if(character <= 122 && character >= 97) {
-      index++;
-      size++;
-      palindrome[index] = (char) character;
+    if(character == '\0') {
+      nullcheck = true;
     }
-    else if(character <= 90  && character >= 65) {
-      character += 32;
-      index++;
-      size ++;
-      palindrome[index] = (char) character;
+    if(nullcheck == false) {
+      if(character <= 122 && character >= 97) {
+        index++;
+        size++;
+        palindrome[index] = (char) character;
+      }
+      else if(character <= 90  && character >= 65) {
+        character += 32;
+        index++;
+        size ++;
+        palindrome[index] = (char) character;
+      }
     }
-    
   }
   int reverseIndex = 0;
   for(int i = size; i >=0; i--) {
     reverse[reverseIndex] = palindrome[i];
     reverseIndex++;
   }
-  
-  cout << "Palindrome: " << palindrome << endl;
-  cout << "reverse: " << reverse << endl;
-  
+  if(strcmp(palindrome,reverse) == 0) {
+  cout << "This is a palindrome!" << endl;
+  }
+  else {
+    cout << "This is not a palindrome." << endl;
+    cout << "Palindrome: " << palindrome << endl;
+    cout << "Reverse: " << reverse << endl;
+  }
   return 0;
 }
